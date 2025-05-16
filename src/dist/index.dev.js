0f5ba5605f6e466f1f71fbbc6e6f1618145b1788
Shanice Jordan <shanicejordan@gmail.com>
2025-05-16 15:06:22 +0200

    added typewriter

--- src/dist/index.dev.js
+++ src/dist/index.dev.js
@@ -1,3 +1,14 @@
 "use strict";
 
-alert("Hello world");
+function generatePoem(event) {
+  event.preventDefault();
+  new Typewriter("#poem", {
+    strings: "la tombe dit a la rose",
+    autoStart: true,
+    delay: 1,
+    cursor: ""
+  });
+}
+
+var poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-alert("Hello world");
+function generatePoem(event) {
+    event.preventDefault();
+
+new Typewriter("#poem", {
+  strings: "la tombe dit a la rose",
+  autoStart: true,
+  delay: 1,
+  cursor: "",
+});
+}
+
+
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
